5881157e70b0d264fc504cf2370acc7a83f44e76
Vicky <vicky@JV-Mac2012.local>
2020-08-05 23:13:20 -0700

    React-Redux: create individual states and combine them in a store instead of using one reducer function

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,9 @@
-import {DISHES} from '../shared/dishes';
-import {COMMENTS} from '../shared/comments';
-import {PROMOTIONS} from '../shared/promotions';
-import {LEADERS} from '../shared/leaders';
+// import {data}
+
+
+
+
+//Reducer is a pure function that takes current state and action, and return a new state. 
 
 export const initialState = {
     dishes: DISHES,
