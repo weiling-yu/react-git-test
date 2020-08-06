93a7e6dc77c242169f2f4abb3289c3d39bb2d244
Vicky <vicky@JV-Mac2012.local>
2020-08-06 15:42:21 -0700

    Rev.sa3: comment form. Creat initial RRF and configure store. Use RRF so the form is persisted and action reset after submit

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import Aboutus from './AboutusComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
     return {
@@ -25,7 +26,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 
 });
 
@@ -70,7 +72,7 @@ class Main extends Component{
         <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={()=> <Menu dishes={this.props.dishes}/>} />
             <Route path="/menu/:dishId" component={DishWithId} isModalOpen={this.props.isModalOpen} />
-            <Route exact path="/contactus" component={Contact} />
+            <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
             <Route exact path="/aboutus" component={()=> <Aboutus leaders={this.props.leaders}/>} />
             <Redirect to="/home"/>
         </Switch>
